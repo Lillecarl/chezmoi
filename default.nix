5da17aec4388271483db29dad7f08a45397eadfd
Carl Hjerpe <git@hjerpe.xyz>
2022-01-10 17:00:01 +0100

    Enable declarative nix user packages

---
+++ default.nix
@@ -0,0 +1,17 @@
+# default.nix
+let
+  pkgs = import <nixpkgs> {};
+
+  packages = with pkgs;
+    [
+      # Sourced directly from Nixpkgs
+      curl
+      htop
+      nix
+      tree
+      terraform
+      keychain
+      powershell
+    ];
+
+in packages
