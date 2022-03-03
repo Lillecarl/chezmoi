6e1f79f43c83d0d0fa7351d9fed209dcc16a3b79
Carl Hjerpe <git@hjerpe.xyz>
2022-03-03 22:39:28 +0100

    Fix PowerShell sudo

--- dot_config/powershell/Microsoft.PowerShell_profile.ps1
+++ dot_config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,10 @@
-function Invoke-MySudo { & /usr/bin/env sudo pwsh -command "& $args" }
-set-alias sudo invoke-mysudo
+function Invoke-MySudo {
+  & /usr/bin/env sudo -E pwsh -command "$args"
+}
 
-oh-my-posh --init --shell pwsh --config ~/Code/oh-my-posh/themes/robbyrussel.omp.json | Invoke-Expression
+set-alias sudo Invoke-MySudo
 
-Clear-Host
+# Load starship
+Invoke-Expression (&starship init powershell)
+
+#Clear-Host
